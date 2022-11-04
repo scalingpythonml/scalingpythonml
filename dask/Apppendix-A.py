0d77816f791847f570bb328b1c5833dd8c0906bc
Holden Karau <holden@pigscanfly.ca>
2022-11-04 14:01:24 -0700

    Add appendix A export

---
+++ dask/Apppendix-A.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+from dask.distributed import Client
+
+client = Client("tcp://127.0.0.1:34711")
+client
+
+
+# In[6]:
+
+
+from time import sleep
+
+
+# In[8]:
+
+
+from dask.distributed import progress
+futs = client.map(sleep, range(0, 100))
+progress(futs)
+
+
+# In[ ]:
+
+
+
+
