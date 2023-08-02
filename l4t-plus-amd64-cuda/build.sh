22c4d95898fce43e66dd8554c8a0d8994d8a42de
Holden Karau <holden@pigscanfly.ca>
2023-08-01 20:48:33 -0700

    While CUDA11.4 on x86 does match l4t we can't get the packages we need :/

--- l4t-plus-amd64-cuda/build.sh
+++ l4t-plus-amd64-cuda/build.sh
@@ -1,7 +1,7 @@
 set -ex
-TAG="c21092023"
+TAG="a01082023"
 CROSS_IMAGE="holdenk/cross-cuda-l4t-new:${TAG}"
 RAY_CROSS_IMAGE="holdenk/ray-x86-and-l4t:${TAG}"
 
-# docker buildx build --platform=linux/arm64,linux/amd64  -t ${CROSS_IMAGE} . --push
+docker buildx build --platform=linux/arm64,linux/amd64  -t ${CROSS_IMAGE} . --push
 docker buildx build --platform=linux/arm64,linux/amd64 --build-arg="BASE_IMAGE=${CROSS_IMAGE}" -t ${RAY_CROSS_IMAGE} . --push -f RayDockerfile
